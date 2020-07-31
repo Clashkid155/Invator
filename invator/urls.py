2c625f7be453fb95e3328a6d0179a3436c64de7d
Clashkid155 <frankdiss9@gmail.com>
2020-07-31 18:00:10 +0100

    Add Create client (beta)

--- invator/urls.py
+++ invator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #path('<str:id>/', views.download_to_pdf, name="download_to_pdf"),
    # path('preview/<str:id>', views.preview_template, name="preview_template"),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('inv/', views.invoice, name="invoice"),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
