5df6ab149584239d6b627d32c80d58af37390dc3
Clashkid155 <frankdiss9@gmail.com>
2020-08-02 11:52:15 +0100

    Update settings

--- root/settings.py
+++ root/settings.py
@@ -12,13 +12,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from dotenv import load_dotenv
-load_dotenv()
+import dj_database_url
+import dotenv
+#from dotenv import load_dotenv
+#load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, "env")
+dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
@@ -94,6 +96,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {}
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -164,5 +167,5 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 LOGIN_REDIRECT_URL = '../../../../dashboard'
 
 django_heroku.settings(locals())
-import dj_database_url
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+options = DATABASES["default"].get("OPTIONS", {})
+options.pop("sslmode", None)
